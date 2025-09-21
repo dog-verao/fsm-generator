076380573d59b0e780adbc8560b31c7405e77152
dog-verao <contatoaguiarti@gmail.com>
2025-09-21 11:07:52 -0600

    feat: refactor modThree to include ops now

--- src/modThree.ts
+++ src/modThree.ts
@@ -1,9 +1,23 @@
-import { FSM } from "./fsm";
+import { FSM, TransitionMap } from './fsm';
+
+type MState = 'S0' | 'S1' | 'S2';
+type MSymbol = '0' | '1';
+
+const transitions: TransitionMap<MState, MSymbol> = {
+  S0: { '0': 'S0', '1': 'S1' },
+  S1: { '0': 'S2', '1': 'S0' },
+  S2: { '0': 'S1', '1': 'S2' },
+};
+
+const mod3FSM = new FSM<MState, MSymbol>({
+  states: ['S0', 'S1', 'S2'],
+  alphabet: ['0', '1'],
+  initialState: 'S0',
+  transitions,
+});
 
 export function modThree(input: string): number {
-  const fsm = new FSM();
-
-  const finalState = fsm.run(input);
+  const finalState = mod3FSM.run(input);
   switch (finalState) {
     case 'S0':
       return 0;
